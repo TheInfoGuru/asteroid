89e5b72107374229a8b29aee3d24fa553f7118aa
TheInfoGuru <scates09@gmail.com>
2024-08-31 00:09:59 -0500

    BUILDING: Created main.py and requirements files

---
+++ main.py
@@ -0,0 +1,10 @@
+# this allows us to use code from
+# the open-source pygame library
+# throughout this file
+import pygame # type: ignore
+
+def main():
+    print('Starting asteroids!')
+
+if __name__ == "__main__":
+    main()
