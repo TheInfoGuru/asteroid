e87e5479ea8189c3d061cc9ca84a3e0a2ffbfef9
TheInfoGuru <scates09@gmail.com>
2024-09-04 22:03:23 -0500

    BUILDING: Added the drawn player, player updating, and player rotation.

--- main.py
+++ main.py
@@ -1,20 +1,26 @@
 import pygame
 from constants import *
+from player import Player
+
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode(size=(SCREEN_WIDTH, SCREEN_HEIGHT))
     game_clock = pygame.time.Clock()
+    player = Player(x=(SCREEN_WIDTH / 2), y=(SCREEN_HEIGHT / 2))
     dt = 0
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-
-        screen.fill(color=(BLACK))
+        
+        player.update(dt=dt)
+        screen.fill(color=BLACK)
+        player.draw(screen=screen)
         pygame.display.flip()
 
+        # limit the framerate to 60 FPS
         dt = game_clock.tick(60) / 1000
         
 
